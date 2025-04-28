cf0bf17f44f4398a4413a57e0a747bc6852bec8a
umar.s <2377328@cmontmorency.qc.ca>
2025-04-28 19:26:04 -0400

    fix: liaison entre user et post créee

--- backend/controllers/posts-controller.js
+++ backend/controllers/posts-controller.js
@@ -1,107 +1,137 @@
-import {validationResult} from 'express-validator';
+import { validationResult } from 'express-validator';
 import HttpError from '../util/http-error.js';
 import Post from '../models/post.js';
+import User from '../models/user.js';
+import mongoose from 'mongoose';
 
 const getPosts = async (req, res, next) => {
-    const posts = await Post.find().exec();
-    if (!posts) {
-        return next(new HttpError('Aucune publication', 404));
-    }
-    res.json(posts);
-}
+  const posts = await Post.find().exec();
+  if (!posts) {
+    return next(new HttpError('Aucune publication', 404));
+  }
+  res.json(posts);
+};
 
 const getPostById = async (req, res, next) => {
-    const postId = req.params.postId;
-    let post;
-    try {
-        post = await Post.findById(postId)
-    }
-    catch (err) {
-        return next(new HttpError('Erreur de récupération de la publication', 500));
-    }
-    if (!post) {
-        return next(new HttpError('Publication introuvable', 404));
-    }
-    res.json({posts: post.toObject({ getters: true })});
-}
-
+  const postId = req.params.postId;
+  let post;
+  try {
+    post = await Post.findById(postId);
+  } catch (err) {
+    return next(new HttpError('Erreur de récupération de la publication', 500));
+  }
+  if (!post) {
+    return next(new HttpError('Publication introuvable', 404));
+  }
+  res.json({ posts: post.toObject({ getters: true }) });
+};
 
 const getPostByUserId = async (req, res, next) => {
-    const userId = req.params.userId;
-    let postsForUser;
-    try {
-        postsForUser = await Post.find({ userId: userId })
-    }
-    catch (err) {
-        return next(new HttpError('Erreur de récupération des publications pour le user', 500));
-    }
-    if (!postsForUser) {
-        return next(new HttpError('Publications introuvables pour le user', 404));
-    }
-    res.json({posts: postsForUser.map(post => post.toObject({ getters: true }))});
-}
+  const userId = req.params.userId;
+  let postsForUser;
+  try {
+    postsForUser = await Post.find({ userId: userId });
+  } catch (err) {
+    return next(new HttpError('Erreur de récupération des publications pour le user', 500));
+  }
+  if (!postsForUser) {
+    return next(new HttpError('Publications introuvables pour le user', 404));
+  }
+  res.json({ posts: postsForUser.map(post => post.toObject({ getters: true })) });
+};
 
 const createPost = async (req, res, next) => {
-    const validationErrors = validationResult(req);
-    if (!validationErrors.isEmpty()) {
-        return next(new HttpError('Données entrées invalides.', 422));
+  const validationErrors = validationResult(req);
+  if (!validationErrors.isEmpty()) {
+    return next(new HttpError('Données entrées invalides.', 422));
+  }
+
+  const { userId, caption, image, location } = req.body;
+
+  let user;
+  try {
+    user = await User.findById(userId);
+    if (!user) {
+      return next(new HttpError('Utilisateur non trouvé.', 404));
     }
-    const { userId, caption, image, location } = req.body; 
-    const createdPost = new Post({
-        userId,
-        caption,
-        image,
-        location, 
-        likes: [],
-        comments: [],
-        creationTime: Date.now()
-    });
-    try {
-        await createdPost.save();
-    }
-    catch (err) {
-        return next(new HttpError('Erreur de création de la publication', 500));
-    }
-    res.status(201).json({ post: createdPost.toObject({ getters: true }) });
-}
+  } catch (err) {
+    return next(new HttpError('Erreur lors de la recherche de l\'utilisateur.', 500));
+  }
+
+  const createdPost = new Post({
+    userId,
+    caption,
+    image,
+    location,
+    likes: [],
+    comments: [],
+    creationTime: Date.now()
+  });
+
+  try {
+    const session = await mongoose.startSession();
+    session.startTransaction();
+    await createdPost.save({ session });
+    user.posts.push(createdPost);
+    await user.save({ session });
+    await session.commitTransaction();
+  } catch (err) {
+    return next(new HttpError('Erreur lors de la création du post.', 500));
+  }
+
+  res.status(201).json({ post: createdPost.toObject({ getters: true }) });
+};
 
 const updatePost = async (req, res, next) => {
-    const postId = req.params.postId;
-    const {caption} = req.body;
+  const postId = req.params.postId;
+  const { caption } = req.body;
 
-    try {
-        const updatedPost = await Post.findByIdAndUpdate(
-            postId,
-            { caption: caption },
-            { new: true }
-        );
-        if (!updatedPost) {
-            return next(new HttpError('Publication introuvable', 404));
-        }
-        res.status(200).json({ post: updatedPost.toObject({ getters: true }) });
+  try {
+    const updatedPost = await Post.findByIdAndUpdate(
+      postId,
+      { caption: caption },
+      { new: true }
+    );
+    if (!updatedPost) {
+      return next(new HttpError('Publication introuvable', 404));
     }
-    catch (err) {
-        return next(new HttpError('Erreur de mise à jour de la publication', 500));
-    }
-}
+    res.status(200).json({ post: updatedPost.toObject({ getters: true }) });
+  } catch (err) {
+    return next(new HttpError('Erreur de mise à jour de la publication', 500));
+  }
+};
+
 const deletePost = async (req, res, next) => {
-    const postId = req.params.postId;
-    try {
-        const post = await Post.findByIdAndDelete(postId);
-        if (!post) {
-            return next(new HttpError('Publication introuvable', 404));
-        }
-        res.status(200).json({ message: 'Publication supprimée' });
+  const postId = req.params.postId;
+  let post;
+  try {
+    post = await Post.findById(postId).populate('userId');
+    if (!post) {
+      return next(new HttpError('Publication introuvable', 404));
     }
-    catch (err) {
-        return next(new HttpError('Erreur de suppression de la publication', 500));
-    }
-}   
+  } catch (err) {
+    return next(new HttpError('Erreur de recherche du post', 500));
+  }
+
+  try {
+    const session = await mongoose.startSession();
+    session.startTransaction();
+    await post.deleteOne({ session });
+    post.userId.posts.pull(post);
+    await post.userId.save({ session });
+    await session.commitTransaction();
+  } catch (err) {
+    return next(new HttpError('Erreur de suppression de la publication', 500));
+  }
+
+  res.status(200).json({ message: 'Publication supprimée' });
+};
+
 export default {
-    getPosts,
-    getPostById,
-    getPostByUserId,
-    createPost,
-    updatePost,
-    deletePost
-}
+  getPosts,
+  getPostById,
+  getPostByUserId,
+  createPost,
+  updatePost,
+  deletePost
+};
--- backend/models/user.js
+++ backend/models/user.js
@@ -3,17 +3,16 @@ import mongoose from "mongoose";
 const userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  favoriteTeam: { type: String, default: "" }, 
+  favoriteTeam: { type: String, default: "" },
   profilePic: { type: String, default: "" },
   followers: { type: Array, default: [] },
   following: { type: Array, default: [] },
   biographie: { type: String, default: "" },
-  posts : [
-    {
-      type: String,
-      required: true,
-    }
-  ]
+  posts: [{ 
+    type: mongoose.Types.ObjectId, 
+    ref: 'Post',
+    required: true 
+  }]
 });
 
 export default mongoose.model("User", userSchema);
